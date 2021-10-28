7d7e4b16228920ae03a54826f2930b2b77a70875
Shanmukha <shanmukhads7@gmail.com>
2021-10-28 23:19:27 +0530

    perfect number code

---
+++ perfect_number.py
@@ -0,0 +1,25 @@
+
+def main():
+    num = int(input('Enter the number: '))
+    if is_perfect(num):
+        print(f'{num} is a perfect number')
+    else:
+        print(f'{num} is not perfect number')
+
+
+def is_perfect(num):
+    if num == result(num):
+        return True
+    return False
+
+
+def result(num):
+    sum = 0
+    for n in range(1, num):
+        if num % n == 0:
+            sum += n
+    return sum 
+
+
+if __name__ == "__main__":
+    main()
