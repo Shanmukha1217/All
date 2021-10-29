6693a703e15c17e85ed096c6fc6e32eda5ed75f2
Shanmukha <shanmukhads7@gmail.com>
2021-10-29 08:46:37 +0530

    code for checking for strong numbers

--- strong_num_series.py
+++ strong_num_series.py
@@ -1,21 +1,27 @@
 import strong_num
 
+def main():
 
-def is_strong(num)-> bool:
-    if num ==strong_num.result(num):
-        return True
-    return False
+    def is_strong(num)-> bool:
+        if num ==strong_num.result(num):
+            return True
+        return False
 
 
-def strong_serie(upper):
-    count = int()
-    num =1
-    while count < upper:
-        if is_strong(num):
-            print(f'{num} is a strong number')
-            count += 1
-        num += 1
-    print(count)
+    def strong_serie(upper):
+        count = int()
+        num =1
+        while count < upper:
+            if is_strong(num):
+                print(f'{num} is a strong number')
+                count += 1
+            num += 1
+        print(count)
 
-upper = int(input('Enter the upper limit: '))
-strong_serie(upper)
+    upper = int(input('Enter the upper limit: '))
+    strong_serie(upper)
+
+
+if __name__ == '__main__':
+    main()
+
