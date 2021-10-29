316b077d43e0cd5e740568d813c692dc4d8f879e
Shanmukha <shanmukhads7@gmail.com>
2021-10-29 08:42:08 +0530

    perfect number series code

---
+++ perfect_series.py
@@ -0,0 +1,13 @@
+from common import read
+from perfect_number import is_perfect
+
+def main():
+    lower = read('Enter the lower range.\n')
+    upper = read('Enter the upper range.\n')
+    print(f'\nPerfect numbers within the range ({lower},{upper}) are:')
+    for i in range(lower, upper) :
+        if is_perfect(i) :
+            print(f'{i}')
+
+if __name__ == '__main__' :
+    main()
